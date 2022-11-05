3c1d682fe7dffcea34b7e162b9232d6a951938f7
belo355 <edilson.belo@live.com>
2022-11-04 21:41:54 -0300

    update swagger

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO CATEGORIA(name, description) VALUES('Massas', 'pratos feitos com massas italianas');
+INSERT INTO CATEGORIA(name, description) VALUES('Hamburguer', 'pratos feitos com os melhores cortes bovinos');
+INSERT INTO CATEGORIA(name, description) VALUES('Sobremesas', 'Deliciosas sobremesas');
